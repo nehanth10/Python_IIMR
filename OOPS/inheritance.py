e623f4db69ccda537e2cf8cf626f12ff7e81c6f1
nehanth <ccrecruiter462.vr@gmail.com>
2024-01-05 19:14:43 +0530

    created inheritance file

---
+++ OOPS/inheritance.py
@@ -0,0 +1,17 @@
+# Python Inheritance
+
+# Inheritance allows us to define a class that inherits all the methods and properties from another class.
+
+# Parent class is the class being inherited from, also called base class.
+
+# Child class is the class that inherits from another class, also called derived class.
+
+# To create a class that inherits the functionality from another class, send the parent class as a parameter when creating the child class.
+
+'''
+class <class_name>:
+    def __init__(self,<parameters>):
+        self.
+        .....
+
+'''
